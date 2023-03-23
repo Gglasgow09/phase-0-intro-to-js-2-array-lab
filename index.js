28b60184e06fc32c63dda5cda2238d57ffdb22ca
Gabrielle Glasgow <g.glasgow91@gmail.com>
2023-03-22 22:31:08 -0400

    added comments for what each function does

--- index.js
+++ index.js
@@ -2,41 +2,41 @@
 const cats = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendCat(name) {
-    cats.push("Ralph");
+    cats.push("Ralph"); //.push adds cat 'Ralph' to the end destructively 
     console.log(cats);
 }
 
 function destructivelyPrependCat(name) {
-    cats.unshift("Bob");
+    cats.unshift("Bob"); //.unshift adds 'Bob' to the beginning destructively
     console.log(cats);
 }
 
 function destructivelyRemoveLastCat(name) {
-    cats.pop();
+    cats.pop(); //.pop removes last cat destructively 
     console.log(cats);
 }
 
 function destructivelyRemoveFirstCat(name) {
-    cats.shift();
+    cats.shift(); // .shift removes first cat destructively 
     console.log(cats);
 }
 
 function appendCat(name) {
-    const newName =[...cats, "Broom"];
+    const newName =[...cats, "Broom"]; //used the spread operator to add a new cat nondestructively to the end of the original array
     return newName;
 }
 
 function prependCat(name) {
-    const newName = ["Arnold", ...cats];
+    const newName = ["Arnold", ...cats]; // used the spread operator to make a copy of original array and add a new cat to the beginning of the new array
     return newName;
 }
 
 function removeLastCat(){
-    const newCats = cats.slice(0, cats.length - 1);
+    const newCats = cats.slice(0, cats.length - 1); //.slice is nondestructive way to remove the last cat 
     return newCats;
 }
 
 function removeFirstCat() {
-    const newCats = cats.slice(1);
+    const newCats = cats.slice(1); //nondestructive way to remove the first cat 
     return newCats;
 }
